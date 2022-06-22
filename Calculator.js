a71a71b1d1ef6d5ffcfb44c13d2c37715989db4d
Vishnu <vishnusk@maveric-systems.com>
2022-06-22 08:36:41 +0530

    Adding divide feature

---
+++ Calculator.js
@@ -0,0 +1,5 @@
+function divide() {
+    var num1 = document.getElementById("firstNumber").value;
+    var num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").value = Number(num1) / Number(num2);
+} 
